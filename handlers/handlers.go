5f915d8498ae2f73c1949b1064023805bba973c7
Sam Chen <sam8211112222@gmail.com>
2022-10-28 11:29:22 +0800

    list winner

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,7 +7,7 @@ import (
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
-	res.WriteHeader(http.StatusNotFound)
+	res.WriteHeader(http.StatusNoContent)
 }
 
 // ListWinners returns winners from the list
